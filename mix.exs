d58e80115861152b759b46153da5b6eeea89497c
Richard Cook <blubparadox@gmail.com>
2024-07-11 22:49:29 -0400

    Initial commit.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ProdigyObject.MixProject do
     [
       app: :prodigy_object,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
