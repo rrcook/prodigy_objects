5d353d47dbec6796c8107306a5863c32f4d8b815
Richard Cook <blubparadox@gmail.com>
2024-07-11 23:23:22 -0400

    Change app to match project name.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ProdigyObject.MixProject do
 
   def project do
     [
-      app: :prodigy_object,
+      app: :prodigy_objects,
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
